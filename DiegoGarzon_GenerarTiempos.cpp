3891cd7f72aa35ed89a65cb16e522e0a549b9804
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-04-03 16:00:04 -0500

    codigo en c++

---
+++ DiegoGarzon_GenerarTiempos.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int fibonacci(int n);
+float get_time(int n);
+int main()
+{
+// Iteracion para encontrar los primero 35 numeros
+for (int i=1;i<36;i++)
+{
+	// El tiempo por cada iteracion
+	float tm=get_time(i);
+	// El valor de fibonacci
+	int N=fibonacci(i);
+	// Imprimir el valor de fibonacci y el tiempo que tardo
+	cout<<N<<","<<tm<<endl;
+}
+
+return 0;
+}
+//Funcion recursiva de fibonacci
+int fibonacci(int n)
+{
+	//Condicional que tiene el caso base
+	if (n==1 || n==0)
+	{
+		//retornar el caso base
+		return n;
+	}
+	//retornar el caso recursivo
+	return fibonacci(n-1)+fibonacci(n-2);
+}
+float get_time(int n)
+{
+// variable de tiempo
+clock_t t=clock();
+// calculo de fibonacci usando la funcion recursiva
+int res=fibonacci(n);
+// determinar el tiempo que se gasto
+t=clock() - t;
+// pasar el tiempo a segundos
+float time = ((float)t)/CLOCKS_PER_SEC;
+// retornar el tiempo en segundos
+return time;
+}
+
+
